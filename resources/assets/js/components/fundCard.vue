1daa4e4919e113a7c6dc3f0954d4c8a4efd2f194
Sor Tasnee <warr@outlook.com>
2016-09-08 15:00:36 +0700

    Tweak some things

--- resources/assets/js/components/fundCard.vue
+++ resources/assets/js/components/fundCard.vue
@@ -3,11 +3,14 @@
     <div class="fundCard-name">
       {{ name }}
     </div>
+    <div class="fundCard-fullName">
+      {{ fullName }}
+    </div>
     <div class="fundCard-nav">
       {{ nav }}
     </div>
     <div class="fundCard-change">
-      ({{ navChange }}%)
+      {{ pipChange }} pips ({{ navChange }}%)
     </div>
   </div>
 </template>
@@ -24,11 +27,19 @@ export default {
 
   data(){ return {
     nav: 0,
-    oldNav: 0
+    oldNav: 0,
+    fullName: ''
   }},
 
   computed: {
+    pipChange(){
+      if(this.nav === 0) return '-';
+      let change = Math.round((this.nav - this.oldNav)*10000);
+      return change > 0 ? '+' + change : change;
+    },
+
     navChange(){
+      if(this.nav === 0) return '-';
       let change = ((this.nav - this.oldNav)*100/this.oldNav).toFixed(4);
       return change > 0 ? '+' + change : change;
     }
@@ -38,6 +49,7 @@ export default {
     Navi.getNav(this.name).then(data => {
       this.nav = data.nav;
       this.oldNav = data.oldNav;
+      this.fullName = data.fullName;
     });
   }
 }
@@ -52,12 +64,16 @@ export default {
   display: inline-block;
 }
 
+.fundCard:first-child{
+  margin-left: 0;
+}
+
 .fundCard-name{
   font-size: 1.5rem;
   text-transform: uppercase;
 }
 
-.fundCard-change{
+.fundCard-fullName, .fundCard-change{
   font-size: 0.7em;
 }
 </style>
--- resources/assets/js/pages/index.vue
+++ resources/assets/js/pages/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="routerWrapper">
-    <fund-card name="kt-st" color="#2196F3"></fund-card>
-    <fund-card name="ktplus" color="#8BC34A"></fund-card>
-    <fund-card name="k-fixed" color="#FF5722"></fund-card>
+    <div class="section">
+      <fund-card name="kt-st" color="#2196F3"></fund-card>
+      <fund-card name="k-fixed" color="#FF5722"></fund-card>
+      <fund-card name="k-cbond" color="#E91E63"></fund-card>
+      <fund-card name="tmbabf" color="#8BC34A"></fund-card>
+    </div>
 
-    <div width="400px" height="500px">
-      <canvas id="chart" width="300" height="300"></canvas>
+    <div class="section">
+      <div width="400px" height="400px">
+        <canvas id="chart" width="300" height="400"></canvas>
+      </div>
     </div>
   </div>
 </template>
@@ -21,45 +26,56 @@ export default{
   ready(){
     Promise.all([
       Navi.getHistoricalChartData('kt-st'),
-      Navi.getHistoricalChartData('ktplus'),
-      Navi.getHistoricalChartData('k-fixed')
+      Navi.getHistoricalChartData('k-fixed'),
+      Navi.getHistoricalChartData('k-cbond'),
+      Navi.getHistoricalChartData('tmbabf')
     ]).then(values => {
       console.log(values);
 
       let ctx = document.getElementById('chart');
       let myChart = new Chart(ctx, {
         type: 'line',
-        data: {
-          datasets: [{
-            label: 'kt-st',
-            data: values[0],
-            fill: false,
-            borderColor: '#2196F3'
-          },{
-            label: 'ktplus',
-            data: values[1],
-            fill: false,
-            borderColor: '#8BC34A'
-          },{
-            label: 'k-fixed',
-            data: values[2],
-            fill: false,
-            borderColor: '#FF5722'
-          }]
-        },
+        data: { datasets: [{
+          label: 'kt-st',
+          data: values[0],
+          fill: false,
+          borderColor: '#2196F3'
+        },{
+          label: 'k-fixed',
+          data: values[1],
+          fill: false,
+          borderColor: '#FF5722'
+        },{
+          label: 'k-cbond',
+          data: values[2],
+          fill: false,
+          borderColor: '#E91E63'
+        },{
+          label: 'tmbabf',
+          data: values[3],
+          fill: false,
+          borderColor: '#8BC34A'
+        }]},
         options: {
           responsive: true,
           maintainAspectRatio: false,
-          scales: { xAxes: [{
-            type: 'time',
-            time: {
-              unit: 'day',
-              displayFormats: { day: 'DD/MM/YY' }
-            },
-            ticks: { callback(label, index, arr){
-              return index % Math.ceil(arr.length/20) === 0 ? label : null;
-            }}
-          }]},
+          scales: {
+            xAxes: [{
+              type: 'time',
+              time: {
+                unit: 'day',
+                displayFormats: { day: 'DD/MM/YY' }
+              },
+              ticks: { callback(label, index, arr){
+                return index % Math.ceil(arr.length/10) === 0 ? label : null;
+              }}
+            }],
+            yAxes: [{
+              ticks: { callback(label, index, arr){
+                return index % Math.ceil(arr.length/5) === 0 ? label.toFixed(4) : null;
+              }}
+            }]
+          },
           legend: { display: false }
         }
       });
