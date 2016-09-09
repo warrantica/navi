b8a8a168073b6823ce077ccfbe1cffdc58f8fb2b
Sor Tasnee <warr@outlook.com>
2016-09-09 10:01:04 +0700

    Create migration and model

--- database/migrations/2016_09_09_024225_create_funds_table.php
+++ database/migrations/2016_09_09_024225_create_funds_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateFundsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('funds', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 20)->unique();
+            $table->string('color', 10);
+            $table->decimal('nav', 10, 4)->default(0);
+            $table->date('navDate');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('funds');
     }
 }
