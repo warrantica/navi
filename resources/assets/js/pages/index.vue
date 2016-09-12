635d897cfdf0a7a77f0659ac5b8ab72e9be5bd6c
Sor Tasnee <warr@outlook.com>
2016-09-12 11:42:44 +0700

    Implement updateChart(months)

--- resources/assets/js/pages/index.vue
+++ resources/assets/js/pages/index.vue
@@ -23,37 +23,28 @@
 
 <script>
 window.Navi = require('../naviAPI.js');
+window.Vars = require('../vars.js');
 
 export default{
   data(){ return{
-    fundData: []
+    fundData: [],
+    chart: {},
+    chartObject: {},
+    chartOptions: {},
+    datasets: []
   }},
 
   methods: {
-
-  },
-
-  ready(){
-
-    let chartPromise = Navi.getAllFunds().then(data => {
-      this.fundData = data;
-      console.log(this.fundData);
-
+    updateChart(numberOfMonths){
       let promises = [];
       for(let fund of this.fundData){
-        promises.push(Navi.getHistoricalChartData(fund.name, 1));
+        promises.push(Navi.getHistoricalChartData(fund.name, numberOfMonths));
       }
 
-      return promises;
-    }).then(promises => {
-      //There's gotta be a better way than to call then on an array of promises
-      //just to all() them again...
       Promise.all(promises).then(values => {
-        console.log(values);
-
-        let datasets = [];
+        this.datasets = [];
         for(let value of values){
-          datasets.push({
+          this.datasets.push({
             label: value.name,
             data: value.chartData,
             fill: false,
@@ -62,57 +53,30 @@ export default{
           });
         }
 
-        Chart.defaults.global.defaultFontColor = '#FFFFFF';
-        Chart.defaults.global.defaultFontFamily = 'Roboto';
-        Chart.defaults.global.defaultFontSize = 14;
-
-        let ctx = document.getElementById('chart');
-        let myChart = new Chart(ctx, {
-          type: 'line', data: { datasets },
-          options: { responsive: true, maintainAspectRatio: false,
-            scales: {
-              xAxes: [{
-                type: 'time',
-                time: {
-                  unit: 'day',
-                  displayFormats: { day: 'DD/MM/YY' }
-                },
-                ticks: {
-                  callback: (label, i, arr) => i % Math.ceil(arr.length/4) === 0 ? label : null,
-                  maxRotation: 0,
-                },
-                gridLines: { color: 'rgba(255,255,255,0.24)', zeroLineWidth: 2, zeroLineColor: '#FFFFFF' }
-              }],
-              yAxes: [{
-                ticks: {
-                  callback: (label, i, arr) => i % Math.ceil(arr.length/5) === 0 ? label.toFixed(4) : null
-                },
-                gridLines: { color: 'rgba(0,0,0,0)', zeroLineWidth: 2, zeroLineColor: '#FFFFFF' }
-              }]
-            },
-            legend: { display: false },
-            tooltips: {
-              mode: 'x-axis',
-              backgroundColor: '#212121',
-              bodySpacing: 10,
-              bodyFontSize: 16,
-              xPadding: 10, yPadding: 10,
-              caretSize: 0,
-              cornerRadius: 2,
-              callbacks: {
-                title: (arr,data) => moment(arr[0].xLabel).format('DD/MM/YY'),
-                label: (item,data) => '  '+data.datasets[item.datasetIndex].label+': '+item.yLabel.toFixed(4),
-                labelColor: (item,ctx) => {
-                  return {
-                    borderColor: ctx.data.datasets[item.datasetIndex].borderColor,
-                    backgroundColor: ctx.data.datasets[item.datasetIndex].borderColor
-                  }
-                }
-              }
-            }
-          }
+        this.chart.destroy();
+        this.chart = new Chart(this.chartObject, {
+          type: 'line', data: { datasets: this.datasets },
+          options: this.chartOptions
         });
       });
+    }
+  },
+
+  ready(){
+    Chart.defaults.global.defaultFontColor = '#FFFFFF';
+    Chart.defaults.global.defaultFontFamily = 'Roboto';
+    Chart.defaults.global.defaultFontSize = 14;
+
+    this.chartObject = document.getElementById('chart');
+    this.chartOptions = Vars.historicalLineChartOptions;
+    this.chart = new Chart(this.chartObject, {
+      type: 'line', data: { datasets: [] },
+      options: this.chartOptions
+    });
+
+    Navi.getAllFunds().then(data => {
+      this.fundData = data;
+      this.updateChart(1);
     });
   }
 }
---
+++ resources/assets/js/vars.js
@@ -0,0 +1,45 @@
+export default {
+  historicalLineChartOptions: {
+    responsive: true, maintainAspectRatio: false,
+    scales: {
+      xAxes: [{
+        type: 'time',
+        time: {
+          unit: 'day',
+          displayFormats: { day: 'DD/MM/YY' }
+        },
+        ticks: {
+          callback: (label, i, arr) => i % Math.ceil(arr.length/4) === 0 ? label : null,
+          maxRotation: 0,
+        },
+        gridLines: { color: 'rgba(255,255,255,0.24)', zeroLineWidth: 2, zeroLineColor: '#FFFFFF' }
+      }],
+      yAxes: [{
+        ticks: {
+          callback: (label, i, arr) => i % Math.ceil(arr.length/5) === 0 ? label.toFixed(4) : null
+        },
+        gridLines: { color: 'rgba(0,0,0,0)', zeroLineWidth: 2, zeroLineColor: '#FFFFFF' }
+      }]
+    },
+    legend: { display: false },
+    tooltips: {
+      mode: 'x-axis',
+      backgroundColor: '#212121',
+      bodySpacing: 10,
+      bodyFontSize: 16,
+      xPadding: 10, yPadding: 10,
+      caretSize: 0,
+      cornerRadius: 2,
+      callbacks: {
+        title: (arr,data) => moment(arr[0].xLabel).format('DD/MM/YY'),
+        label: (item,data) => '  '+data.datasets[item.datasetIndex].label+': '+item.yLabel.toFixed(4),
+        labelColor: (item,ctx) => {
+          return {
+            borderColor: ctx.data.datasets[item.datasetIndex].borderColor,
+            backgroundColor: ctx.data.datasets[item.datasetIndex].borderColor
+          }
+        }
+      }
+    }
+  }
+}
