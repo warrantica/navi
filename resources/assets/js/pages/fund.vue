f67906c8665b33dd35364a96e404f8380bd4b90d
Sor Tasnee <warr@outlook.com>
2016-09-14 14:14:20 +0700

    Add text box

--- resources/assets/js/pages/fund.vue
+++ resources/assets/js/pages/fund.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="routerWrapper">
     <div class="section">
-      This is {{ fundData.name }} <br>
-      Nav: {{ fundData.nav }}
+      <input class="nameTextBox"
+             type="text"
+             v-model="currentFundName"
+             @keyup.enter="goToFund">
     </div>
     <div class="section">
       <performance-graph></performance-graph>
@@ -13,12 +15,24 @@
 <script>
 export default {
   data(){ return {
+    currentFundName: this.$route.params.fundname,
     fundData: {}
   }},
 
+  methods: {
+    updateData(){
+      Navi.getNav(this.$route.params.fundname).then(data => this.fundData = data);
+      this.$broadcast('updateChart', [this.$route.params.fundname, 'ktplus']);
+    },
+
+    goToFund(){
+      this.$router.go({name: 'fund', params: {fundname: this.currentFundName}});
+      this.updateData();
+    }
+  },
+
   ready(){
-    Navi.getNav(this.$route.params.fundname).then(data => this.fundData = data);
-    this.$broadcast('updateChart', [this.$route.params.fundname]);
+    this.updateData();
   }
 }
 </script>
@@ -26,5 +40,18 @@ export default {
 <style scoped lang="scss">
 @import "../../sass/variables.scss";
 
-
+.nameTextBox{
+  display: block;
+  box-sizing: border-box;
+  padding: 10px 20px;
+  margin: 10px 0;
+  background: $grey;
+  @include grey-border;
+  width: 100%;
+  font: 2rem $main-font;
+  font-weight: $font-weight-light;
+  color: $white;
+  border-radius: 2px;
+  text-transform: uppercase;
+}
 </style>
